836bdcdf7882dba7e69b8e9166726f6ee020ec21
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-19 16:44:25 +0000

    Deploy deveeup/HolaMundoMapa to github.com/deveeup/HolaMundoMapa.git:gh-pages

---
+++ precache-manifest.66cb4ca5f6b8bf87d9224c88e69c4ed1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d90397497ff88d955a0a2c31a47c69",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e95915ddefa7baa1168",
+    "url": "./static/css/main.409a4fcd.chunk.css"
+  },
+  {
+    "revision": "1dd6ccc1de27df434d3c",
+    "url": "./static/js/2.88d88762.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "./static/js/2.88d88762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e95915ddefa7baa1168",
+    "url": "./static/js/main.26b21f1c.chunk.js"
+  },
+  {
+    "revision": "65c8d061a75573fc3d15",
+    "url": "./static/js/runtime-main.ff8a6579.js"
+  },
+  {
+    "revision": "8c3b201f5f1ace6599341eea326c30bd",
+    "url": "./static/media/icon.8c3b201f.svg"
+  },
+  {
+    "revision": "314b00b9f9345bb0b91a341f97c98cb1",
+    "url": "./static/media/notFound.314b00b9.gif"
+  }
+]);
