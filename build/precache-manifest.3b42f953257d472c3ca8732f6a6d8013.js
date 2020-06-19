54022c78b50656a675c786e62e379c66ed0f8b62
David R <david.rojas@outsourcing.libertycolombia.com>
2020-06-19 11:42:47 -0500

    update travis

---
+++ build/precache-manifest.3b42f953257d472c3ca8732f6a6d8013.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144e82e24d7ad05090331de84b303721",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ff902413bcb02e3e49d",
+    "url": "./static/css/main.db60eee6.chunk.css"
+  },
+  {
+    "revision": "1dd6ccc1de27df434d3c",
+    "url": "./static/js/2.88d88762.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "./static/js/2.88d88762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ff902413bcb02e3e49d",
+    "url": "./static/js/main.23576083.chunk.js"
+  },
+  {
+    "revision": "65c8d061a75573fc3d15",
+    "url": "./static/js/runtime-main.ff8a6579.js"
+  },
+  {
+    "revision": "8c3b201f5f1ace6599341eea326c30bd",
+    "url": "./static/media/icon.8c3b201f.svg"
+  },
+  {
+    "revision": "314b00b9f9345bb0b91a341f97c98cb1",
+    "url": "./static/media/notFound.314b00b9.gif"
+  }
+]);
